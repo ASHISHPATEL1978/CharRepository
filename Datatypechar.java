016cb08dfbe1349fb6633d4b881185976364220c
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-11-01 23:29:20 +0000

    Char Data Type Commit

---
+++ Datatypechar.java
@@ -0,0 +1,23 @@
+package datatypechar;
+
+
+
+public class Datatypechar
+{
+    public static void main ( String[] args )
+    {
+
+        char A = 30;
+        char B = 5;
+
+        System.out.println ( A + B );
+        System.out.println ( A - B );
+        System.out.println ( A * B );
+        System.out.println ( A / B );
+        System.out.println ( A++ );
+        System.out.println ( B-- );
+        System.out.println ( ++A + --B );
+        System.out.println ( ++A - --B );
+    }
+
+}
